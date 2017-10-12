219543522f0ac72a4613bede4f0eb3f67f39043d
Mammadli Chingiz <memmedlicngz@gmail.com>
2017-10-12 16:01:55 +0400

    added newsActivity

---
+++ app/src/main/java/com/example/chingizmammadli/newspaper/NewsActivity.java
@@ -0,0 +1,17 @@
+package com.example.chingizmammadli.newspaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+
+    protected void getView(){
+
+    }
+}

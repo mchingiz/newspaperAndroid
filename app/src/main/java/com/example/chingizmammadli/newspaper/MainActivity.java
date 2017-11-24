e73bef80654893c0c4d87235b767634a5d786ebd
Mammadli Chingiz <memmedlicngz@gmail.com>
2017-11-24 17:15:01 +0400

    sdf2 done

--- app/src/main/java/com/example/chingizmammadli/newspaper/MainActivity.java
+++ app/src/main/java/com/example/chingizmammadli/newspaper/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.chingizmammadli.newspaper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +16,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public SQLiteDatabase db;
     private static int REGISTRATION_SUCCESS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        db = openOrCreateDatabase("dictionary",MODE_PRIVATE,null);
 
         // Butun click listenerleri bu qayda ile elave olunsun ki, hamisini asagidaki
         // onClick funksiyasindan idare eleyek
@@ -33,6 +40,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             EditText password = (EditText) findViewById(R.id.login_password);
             password.setTypeface(Typeface.DEFAULT);
             password.setTransformationMethod(new PasswordTransformationMethod());
+
+
+        this.createTable();
+//        this.populateDatabase();
+    }
+
+    private void populateDatabase(){
+//        db.execSQL("INSERT INTO words (word,description) VALUES ('research','careful study that is done to find and report new knowledge about something') ");
+    }
+
+    private void createTable(){
+//        db.execSQL("DROP TABLE IF EXISTS words");
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name text NOT NULL, surname text NOT NULL, email text NOT NULL, password text NOT NULL)");
     }
 
     public void onClick(View v){
@@ -45,11 +65,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             }
             case R.id.login_button: {
-                Toast.makeText(this, "Clicked on Login", Toast.LENGTH_LONG).show();
+                EditText emailField = (EditText) findViewById(R.id.login_email);
+                EditText passwordField = (EditText) findViewById(R.id.login_password);
 
-                Intent newsIntent = new Intent(MainActivity.this,NewsActivity.class);
-                startActivity(newsIntent);
-                finish();
+                String email = emailField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
+
+                if(email == null || email.equals("") || password == null || password.equals("")){
+                    Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show();
+
+                }else{
+                    Boolean authenticated = false;
+//                    Toast.makeText(this, "Clicked on Login", Toast.LENGTH_LONG).show();
+
+                    Cursor cursor = db.rawQuery("SELECT * FROM users WHERE email=?",new String[]{email});
+
+                    if(cursor != null & cursor.getCount() > 0){
+                        while(cursor.moveToNext()){
+                            String rowEmail = cursor.getString(cursor.getColumnIndex("email"));
+                            String rowPassword = cursor.getString(cursor.getColumnIndex("password"));
+
+                            if(rowPassword.equals(password)){
+                                authenticated = true;
+                            }
+                        }
+
+                        if(authenticated){
+                            Intent newsIntent = new Intent(MainActivity.this,NewsActivity.class);
+                            startActivity(newsIntent);
+                            finish();
+                        }else{
+                            Toast.makeText(this, "Password is wrong", Toast.LENGTH_LONG).show();
+                        }
+                    }else{
+                        Toast.makeText(this, "No user", Toast.LENGTH_LONG).show();
+                    }
+                }
                 break;
             }
         }
--- app/src/main/java/com/example/chingizmammadli/newspaper/RegisterActivity.java
+++ app/src/main/java/com/example/chingizmammadli/newspaper/RegisterActivity.java
@@ -2,17 +2,24 @@ package com.example.chingizmammadli.newspaper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         // onClick funksiyasindan idare eleyek
         Button registerButton = (Button)findViewById(R.id.submit_button);
         registerButton.setOnClickListener(this);
+
+        db = openOrCreateDatabase("dictionary",MODE_PRIVATE,null);
 
         // Passwordun hinti basqa fontda cixirdi,onu duzeltmisem
         EditText password = (EditText) findViewById(R.id.register_password);
@@ -35,14 +44,83 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     public void onClick(View v){
 
+        EditText nameField = (EditText) findViewById(R.id.register_name);
+        EditText surnameField = (EditText) findViewById(R.id.register_surname);
+        EditText emailField = (EditText) findViewById(R.id.register_email);
+        EditText passwordField = (EditText) findViewById(R.id.register_password);
+        EditText passwordConfirmationField = (EditText) findViewById(R.id.register_passwordConfirmation);
+
+        String name = nameField.getText().toString().trim();
+        String surname = surnameField.getText().toString().trim();
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+        String passwordConfirmation = passwordConfirmationField.getText().toString().trim();
+
         switch(v.getId()){
             case R.id.submit_button: {
-//                Toast.makeText(this, "Clicked on submit", Toast.LENGTH_SHORT).show();
+                Log.v("NEWS",name+surname+email+password+passwordConfirmation);
 
-                Intent intent = new Intent();
-                setResult(1,intent);
-                finish();
+                if(name == null || name.equals("")){
+                    Toast.makeText(this, "Name field cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if(surname == null || surname.equals("")){
+                    Toast.makeText(this, "Surname field cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if(email == null || email.equals("")){
+                    Toast.makeText(this, "Email field cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if((password == null || password.equals("")) || (passwordConfirmation == null || passwordConfirmation.equals(""))){
+                    Toast.makeText(this, "Password and confirmations fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if(!passwordConfirmation.equals(password)){
+                    Toast.makeText(this, "Passwords doesn't match", Toast.LENGTH_SHORT).show();
+                }else if(!this.isEmailValid(email)){
+                    Toast.makeText(this, "Email is not valid", Toast.LENGTH_SHORT).show();
+                }else if(password.length() < 6){
+                    Toast.makeText(this, "Password length at least 6 characters", Toast.LENGTH_SHORT).show();
+                }else if(this.doesUserExist(email)){
+                    Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show();
+                }else{
+                    db.execSQL("INSERT INTO users (name,surname,email,password) VALUES (?,?,?,?)",new String[]{name,surname,email,password});
+
+                    this.printAllUsers();
+
+                    Toast.makeText(this, "User has been created", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent();
+                    setResult(1,intent);
+                    finish();
+                }
             }
+        }
+    }
+
+    private Boolean doesUserExist(String email){
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE email = ?",new String[]{email});
+
+        if(cursor != null & cursor.getCount() > 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private void printAllUsers(){
+        Cursor cursor = db.rawQuery("SELECT * FROM users",null);
+
+        if(cursor != null & cursor.getCount() > 0){
+            while(cursor.moveToNext()){
+//                String username = cursor.getString(cursor.getColumnIndex("name"));
+                String email = cursor.getString(cursor.getColumnIndex("email"));
+                String password = cursor.getString(cursor.getColumnIndex("password"));
+
+                Log.v("NEWS","USER: / "+email+" / "+password);
+            }
+        }else{
+            Log.v("NEWS","NO USER");
         }
     }
 }
